ba3581e2c4511a27e63ff3d0821dfb0f25fe641f
AnsM <ansonmau172@gmail.com>
2025-08-15 16:40:59 -0400

    set up 1

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 def main():
-        driver = webdriver.Chrome("")
+        driver = webdriver.Chrome()
+        print("test3")
         return
 
+print("test1")
+
 if __name__ == "__main__":
+        print("test2")
         main()
