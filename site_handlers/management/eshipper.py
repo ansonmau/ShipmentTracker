fc2079f45785f02cd7b2a8b2e41b03d4e1e87d9a
AnsM <ansonmau172@gmail.com>
2025-08-18 14:51:12 -0400

    further devloping scrape function. currently stuck after loading page

--- site_handlers/management/eshipper.py
+++ site_handlers/management/eshipper.py
@@ -8,11 +8,11 @@ paths = {
         "username_input": (ELEMENT_TYPES['id'], 'username'),
         "password_input": (ELEMENT_TYPES['id'], 'password'),
         "login_button": (ELEMENT_TYPES['css'], '[aria-label="Login Button"]'),
-        "searchBox_input": (ELEMENT_TYPES['css'], 'placeholder="Search"'),
-        "packages_table": (ELEMENT_TYPES['css'], '#cdk-drop-list-2 > tbody:nth-child(2)'),
-        "entries_fromPackagesTable": (ELEMENT_TYPES['css'], 'tr'),
-        "packageNumber_fromEntries": (ELEMENT_TYPES['css'], '[class="link-text"]'),
-        "usernameText": (ELEMENT_TYPES['css'], '.userNameText')
+        "usernameText": (ELEMENT_TYPES['css'], '.userNameText'),
+        "searchBox_input": (ELEMENT_TYPES['css'], '.ng-dirty'),
+        "packages_table": (ELEMENT_TYPES['css'], '.cdk-drop-list'),
+        "entries_fromPackagesTable": (ELEMENT_TYPES['tag'], 'tr'),
+        "packageNumber_fromEntry": (ELEMENT_TYPES['css'], '.link-text'),
 }
 
 
@@ -28,11 +28,13 @@ def login(sesh: WebDriverSession):
 def scrape(sesh: WebDriverSession):
         sesh.get("https://ww2.eshipper.com/customer/tracking")
 
+        sesh.inputText(paths['searchBox_input'], "in transit")
+
         pTable = sesh.find(paths['packages_table'])
         pTable_entries = sesh.findAllFromParent(pTable, paths['entries_fromPackagesTable'])
 
         for entry in pTable_entries:
-                element = sesh.findFromParent(entry, paths['packageNumber_fromEntries'])
+                element = sesh.findFromParent(entry, paths['packageNumber_fromEntry'])
                 pass
         
 
