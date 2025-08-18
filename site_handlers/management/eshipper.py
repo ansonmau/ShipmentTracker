98144c2c07f2d72113396b91b6450129d5f9e3f3
AnsM <ansonmau172@gmail.com>
2025-08-18 04:05:29 -0400

    started working on scraping for necessary info

--- site_handlers/management/eshipper.py
+++ site_handlers/management/eshipper.py
@@ -5,7 +5,9 @@ paths = {
         "username_input": (ELEMENT_TYPES['id'], 'username'),
         "password_input": (ELEMENT_TYPES['id'], 'password'),
         "login_button": (ELEMENT_TYPES['css'], '[aria-label="Login Button"]'),
-        
+        "searchBox_input": (ELEMENT_TYPES['css'], 'placeholder="Search"'),
+        "packages_table": (ELEMENT_TYPES['css'], '#cdk-drop-list-2 > tbody:nth-child(2)'),
+
 }
 
 
@@ -17,5 +19,8 @@ def login(sesh: WebDriverSession):
         sesh.click(paths['login_button'])
 
 def scrape(sesh: WebDriverSession):
-         
-        pass
+        sesh.get("https://ww2.eshipper.com/customer/tracking")
+
+
+        pTable = sesh.find(paths['packages_table'])
+        
