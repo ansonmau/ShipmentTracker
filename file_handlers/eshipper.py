a244816dc6167c049f40f9da17370655d4333a49
AnsM <ansonmau172@gmail.com>
2025-08-20 15:31:17 -0400

    feat: adds csv handler for eshipper

---
+++ file_handlers/eshipper.py
@@ -0,0 +1,27 @@
+import glob
+import csv
+import pathlib
+
+def parse():
+        dl_files = getFiles()
+
+        assert len(dl_files) > 0
+        
+        # should only be one file
+        csv_file = dl_files[0]
+        
+
+        pass
+
+def check():
+        files = getFiles()
+        return True if len(files)>0 else False
+
+def getFiles():
+        proj_folder = pathlib.Path(__file__).resolve().parent.parent
+        dl_folder = proj_folder / 'dls'
+
+        files = dl_folder.glob("Track*.csv") # returns generator
+        files = list(files)
+
+        return files
