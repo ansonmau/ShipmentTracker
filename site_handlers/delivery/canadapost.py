10378af2c75a31e5b8584118f55922625853d72c
AnsM <ansonmau172@gmail.com>
2025-08-20 23:57:01 -0400

    feat: adding basic logic

---
+++ site_handlers/delivery/canadapost.py
@@ -0,0 +1,11 @@
+from core.driver import WebDriverSession, ELEMENT_TYPES
+from os import getenv
+from core.log import getLogger
+
+
+def executeScript(sesh: WebDriverSession, tracking_nums):
+        for tNum in tracking_nums:
+                link = "https://www.canadapost-postescanada.ca/track-reperage/en#/search?searchFor={}".format(tNum)
+                sesh.get(link)
+
+                
