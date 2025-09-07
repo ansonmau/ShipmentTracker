564611206184654f05228320c2f45643f66d6799
ansonmau <ansonmau172@gmail.com>
2025-09-07 17:55:35 -0400

    mute uc debug logging while keeping mine

--- core/log.py
+++ core/log.py
@@ -2,12 +2,14 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
+    format="%(asctime)s %(name)s: [%(levelname)s] %(message)s",
     handlers=[
         logging.FileHandler("output.log"), 
         logging.StreamHandler() 
     ]
 )
+
+logging.getLogger("undetected_chromedriver").setLevel(logging.WARNING)
 
 def getLogger(name):
         return logging.getLogger(name)
