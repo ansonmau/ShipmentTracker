d35c3b872701ea121ce1ce481ef6ce24c359ec7b
ansonmau <ansonmau172@gmail.com>
2025-09-09 01:10:33 -0400

    feat: logs to folder ./logs and makes each log file unique

--- core/log.py
+++ core/log.py
@@ -1,10 +1,15 @@
 import logging
+from datetime import datetime
+import os
+
+time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+log_filename = os.path.join("./logs", "log_{}.log".format(time))
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s %(name)s: [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("output.log"), 
+        logging.FileHandler(log_filename), 
         logging.StreamHandler() 
     ]
 )
