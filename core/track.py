6bcee9d5497ab99ffaacf07dc5a4bebfc7dcc1e3
ansonmau <ansonmau172@gmail.com>
2025-09-11 00:16:38 -0400

    feat: result object that can be used by site handlers

--- core/track.py
+++ core/track.py
@@ -3,6 +3,10 @@ from core.log import getLogger
 
 logger = getLogger(__name__)
 
+SUCCESS = 0
+FAIL = 1
+CRASH = 2
+
 def track(sesh: WebDriverSession, tracking_nums, executeScript):
         report = {
                 "success": [],
@@ -17,12 +21,25 @@ def track(sesh: WebDriverSession, tracking_nums, executeScript):
                 counter += 1
                 logger.info(f"Attempting tracking for #{tracking_num} ({counter}/{total_count})")
                 try:
-                        if executeScript(sesh, tracking_num):
-                                report['success'].append(tracking_num)
+                        curr_result = executeScript(sesh, tracking_num)
+                        if curr_result == SUCCESS:
+                                report['success'].append((tracking_num, curr_result))
                         else:
-                                report['fail'].append(tracking_num)
+                                report['fail'].append((tracking_num, curr_result))
                 except Exception as e:
+                        curr_result = result(CRASH, "Unknown error occured")
                         logger.warning("(#{}) Unknown error: {}".format(tracking_num, e))
-                        report['crash'].append(tracking_num)
+                        report['crash'].append((tracking_num, curr_result))
 
         return report
+
+class result:
+        def __init__(self, result, reason = ""):
+                self.result = result
+                self.reason = reason
+        
+        def __str__(self):
+                return f"{self.result}: {self.reason}"
+        
+        def __eq__(self, other):
+                return self.result == other
