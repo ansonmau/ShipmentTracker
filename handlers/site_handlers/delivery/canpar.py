9fab48b7b6325c6bb6e6050880d7fd62862d4ad2
ansonmau <ansonmau172@gmail.com>
2025-10-03 18:54:00 -0400

    fix (canpar): works now

--- handlers/site_handlers/delivery/canpar.py
+++ handlers/site_handlers/delivery/canpar.py
@@ -1,7 +1,8 @@
 from core.driver import WebDriverSession, ELEMENT_TYPES
 from os import getenv
 from core.log import getLogger
-from time import time as current_time
+import time
+from core.track import result
 
 logger = getLogger(__name__)
 
@@ -10,7 +11,7 @@ paths = {
         ELEMENT_TYPES["id"],
         "options-block_5f6a6725bde09-accordion-4-heading",
     ),
-    "notify_exception_toggle": (ELEMENT_TYPES["id"], "notifyAtException"),
+    "notify_exception_toggle": (ELEMENT_TYPES["css"], '[for="notifyAtException"]'),
     "email_input": (ELEMENT_TYPES["id"], "notifyList"),
     "add_notification_btn": (
         ELEMENT_TYPES["css"],
@@ -18,23 +19,6 @@ paths = {
     ),
     "notification_section": (ELEMENT_TYPES["id"], "notifyContent"),
 }
-
-
-def track(sesh: WebDriverSession, tracking_nums):
-    report = {"success": [], "fail": [], "crash": []}
-
-    for tracking_num in tracking_nums:
-        try:
-            if executeScript(sesh, tracking_num):
-                report["success"].append(tracking_num)
-            else:
-                report["fail"].append(tracking_num)
-        except Exception as e:
-            logger.warning("(#{}) Unknown error: {}".format(tracking_num, e))
-            report["crash"].append(tracking_num)
-
-    return report
-
 
 def executeScript(sesh: WebDriverSession, tracking_num):
     sesh.get(
@@ -44,23 +28,26 @@ def executeScript(sesh: WebDriverSession, tracking_num):
     )
 
     notify_me = sesh.find.path(paths["notify_me_btn"])
-    sesh.scrollToElement(notify_me)
     sesh.click.element(notify_me)
+    sesh.scrollToElement(notify_me)
+    time.sleep(1)
     sesh.click.path(paths["notify_exception_toggle"])
-    sesh.input.path(paths["email_input"], getenv("CANPAR_EMAIL"))
+    
+    email_input_txt = "{}\n{}".format(getenv("CANPAR_EMAIL1"), getenv("CANPAR_EMAIL2")) 
+    sesh.input.path(paths["email_input"], email_input_txt)
     sesh.click.path(paths["add_notification_btn"])
 
     waitForConfirm(sesh)
 
-    return True
+    return result.SUCCESS
 
 
 def waitForConfirm(sesh: WebDriverSession, cd=10):
     confirmation_text = "Thank you. Notifications have been updated."
 
-    end_time = current_time() + cd
+    end_time = time.time() + cd
 
-    while current_time() < end_time:
+    while time.time() < end_time:
         if sesh.read.text(paths["notification_section"]) == confirmation_text:
             return True
 
