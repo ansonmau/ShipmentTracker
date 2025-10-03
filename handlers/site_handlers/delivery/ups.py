7d34cf92adc0757804eb0cdda121650520d983bd
ansonmau <ansonmau172@gmail.com>
2025-10-03 19:55:24 -0400

    fix (ups): works now

--- handlers/site_handlers/delivery/ups.py
+++ handlers/site_handlers/delivery/ups.py
@@ -1,53 +1,70 @@
 from core.driver import WebDriverSession, ELEMENT_TYPES
 from os import getenv
 from core.log import getLogger
+from core.track import result
+import time
 
 logger = getLogger(__name__)
 
-paths = {
-    "notify_me_btn": (ELEMENT_TYPES["id"], "stApp_btnSendMeUpdate"),
-    "continue_btn": (ELEMENT_TYPES["css"], ".ups-cta_primary"),
-    "update_option_1": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesDelaylbl"),
-    "update_option_2": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesDeliveredlbl"),
-    "update_option_3": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesCurStatusLbl"),
-    "email_input_1": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesEmailPhone0"),
-    "update_option_4": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesNotifyProblemLbl"),
-    "email_input_2": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesfailureEmail"),
-    "done_btn": (ELEMENT_TYPES["id"], "stApp_sendUpdateDoneBtn"),
-    "close_btn": (ELEMENT_TYPES["id"], "stApp_notiComplete_closeBtn"),
-}
+class Paths:
+    cookies_popup = (ELEMENT_TYPES["id"], "onetrust-consent-sdk")
 
+    homepage = {
+        "notify_me_btn": (ELEMENT_TYPES["id"], "stApp_btnSendMeUpdate"),
+        }
 
-def track(sesh: WebDriverSession, tracking_nums):
-    report = {"success": [], "fail": [], "crash": []}
+    popup = {
+        "continue_btn": (ELEMENT_TYPES["css"], ".ups-cta_primary"),
+        "email_input_1": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesEmailPhone0"),
+        "email_input_2": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesfailureEmail"),
+        "done_btn": (ELEMENT_TYPES["id"], "stApp_sendUpdateDoneBtn"),
+        "close_btn": (ELEMENT_TYPES["id"], "stApp_notiComplete_closeBtn"),
+        }
 
-    for tracking_num in tracking_nums:
-        try:
-            if executeScript(sesh, tracking_num):
-                report["success"].append(tracking_num)
-            else:
-                report["fail"].append(tracking_num)
-        except Exception as e:
-            logger.warning("(#{}) Unknown error: {}".format(tracking_num, e))
-            report["crash"].append(tracking_num)
-
-    return report
-
+    popup_options = {
+        "update_option_1": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesDelaylbl"),
+        "update_option_2": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesDeliveredlbl"),
+        "update_option_3": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesCurStatusLbl"),
+        "update_option_4": (ELEMENT_TYPES["id"], "stApp_pkgUpdatesNotifyProblemLbl"),
+        }
 
 def executeScript(sesh: WebDriverSession, tracking_num):
-    sesh.get("https://www.ups.com/WebTracking?trackingNumber={}".format(tracking_num))
+    sesh.get("https://www.ups.com/track?trackingNumber={}".format(tracking_num))
+    
+    remove_cookies_popup(sesh)
 
-    notify_btn = sesh.find.path(paths["notify_me_btn"])
-    sesh.scrollToElement(notify_btn)
+    notify_btn = sesh.find.path(Paths.homepage["notify_me_btn"])
+    sesh.scrollToElement(notify_btn, centered=True)
+    time.sleep(1)
     sesh.click.element(notify_btn)
-    sesh.click.path(paths["continue_btn"])
-    sesh.click.path(paths["update_option_1"])
-    sesh.click.path(paths["update_option_2"])
-    sesh.click.path(paths["update_option_3"])
-    sesh.click.path(paths["update_option_4"])
-    sesh.input.path(paths["email_input_1"], getenv("UPS_EMAIL1"))
-    sesh.input.path(paths["email_input_2"], getenv("UPS_EMAIL2"))
-    sesh.click.path(paths["done_btn"])
-    sesh.waitFor.path(paths["close_btn"])
 
-    return True
+    sesh.click.path(Paths.popup["continue_btn"])
+
+    elms_options = get_options(sesh)
+    for elm in elms_options:
+        sesh.click.element(elm)
+
+    sesh.input.path(Paths.popup["email_input_1"], getenv("UPS_EMAIL1"))
+    sesh.input.path(Paths.popup["email_input_2"], getenv("UPS_EMAIL2"))
+
+    sesh.click.path(Paths.popup["done_btn"])
+    sesh.waitFor.path(Paths.popup["close_btn"])
+
+    return result.SUCCESS
+ 
+def remove_cookies_popup(sesh):
+    elm_popup = sesh.find.path(Paths.cookies_popup)
+    sesh.remove_element(elm_popup)
+
+def get_options(sesh:WebDriverSession):
+    opt_elm_list = []
+
+    for opt_path in Paths.popup_options.values():
+        elm_opt = sesh.find.path(opt_path)
+        if elm_opt:
+            opt_elm_list.append(elm_opt)
+
+    return opt_elm_list
+
+            
+
