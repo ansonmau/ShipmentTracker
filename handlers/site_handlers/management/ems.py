481f02005814543534930ed89cfd237e021c9e87
ansonmau <ansonmau172@gmail.com>
2025-10-08 19:59:53 -0400

    fix(ems): went to wrong site before >:(

--- handlers/site_handlers/management/ems.py
+++ handlers/site_handlers/management/ems.py
@@ -5,14 +5,10 @@ from core.log import getLogger
 logger = getLogger(__name__)
 
 class Paths:
-    homepage = {
-            "login_btn": (ELEMENT_TYPES['css'], '.app-login')
-            }
-
     login = {
-            "username": (ELEMENT_TYPES['id'], 'username'),
-            "password": (ELEMENT_TYPES['id'], 'password'),
-            "login_btn": (ELEMENT_TYPES['css'], '[name="submit"]'),
+            "username": (ELEMENT_TYPES['id'], 'email'),
+            "password": (ELEMENT_TYPES['css'], '[name="password"]'),
+            "login_btn": (ELEMENT_TYPES['id'], 'doSubmit'),
             }
 
 
@@ -22,15 +18,9 @@ def scrape(sesh: WebDriverSession):
     return
 
 def login(sesh: WebDriverSession):
-    login_url = "https://www.ems.post/en/global-network/tracking"
-    
+    login_url = "https://emarketplaceservices.com/login"
     sesh.get(login_url)
-    login_btn_elm = sesh.find.path(Paths.homepage["login_btn"])
-    sesh.click.element(login_btn_elm)
     
-    login_btn_2_elm = sesh.find.links_within(login_btn_elm, filter="Login")[0]
-    sesh.click.element(login_btn_2_elm)
-
     sesh.input.path(Paths.login["username"], getenv("EMS_USER"))
     sesh.input.path(Paths.login["password"], getenv("EMS_PW"))
 
