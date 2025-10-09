520301c30113f317ba259e7fb6159fec5e84d7f8
ansonmau <ansonmau172@gmail.com>
2025-10-08 22:33:17 -0400

    feat(ems): starting work on filtering by date

--- handlers/site_handlers/management/ems.py
+++ handlers/site_handlers/management/ems.py
@@ -2,6 +2,8 @@ from core.driver import WebDriverSession, ELEMENT_TYPES
 from os import getenv
 from core.log import getLogger
 
+from datetime import datetime, timedelta
+
 logger = getLogger(__name__)
 
 class Paths:
@@ -11,9 +13,22 @@ class Paths:
             "login_btn": (ELEMENT_TYPES['id'], 'doSubmit'),
             }
 
+    shipment_page = {
+            "shipment_table": (ELEMENT_TYPES['id'], 'shipmentTable'),
+            "date_filter_div": (ELEMENT_TYPES['css'], '.input-daterange'),
+
+            }
+
 
 def scrape(sesh: WebDriverSession):
     login(sesh)
+    
+    shipment_page_url = "https://emarketplaceservices.com/shipments"
+    sesh.get(shipment_page_url)
+
+    s_today, s_from_date = get_filter_dates()
+    
+    
 
     return
 
@@ -26,3 +41,16 @@ def login(sesh: WebDriverSession):
 
     sesh.click.path(Paths.login["login_btn"])
     
+def get_filter_dates():
+    date_format = "%dd/%mm/%Y"
+    s_today = datetime.now().strftime(date_format)
+    
+    from_date = datetime.now() - timedelta(days=3)
+    s_from_date = from_date.strftime(date_format)
+
+    return s_from_date, s_today
+
+def get_filter_inputs(sesh: WebDriverSession):
+    filter_div = sesh.find.path(Paths.shipment_page['date_filter_div'])
+    
+    # find inputs within filter_div -> return individually
