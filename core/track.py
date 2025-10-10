f74a1d644bb049d7be47a19973a93737734a7c80
ansonmau <ansonmau172@gmail.com>
2025-10-10 00:48:43 -0400

    feat(tracking): can now retry, and will not re-attempt failed ones (will re-attempt unexpected errors)

--- core/track.py
+++ core/track.py
@@ -10,8 +10,9 @@ class result:
     SUCCESS = 1
     FAIL = 0
     CRASH = 2
+    RETRY = 3
 
-    def __init__(self, result, reason="")
+    def __init__(self, result, reason=""):
         assert result in [self.SUCCESS, self.FAIL, self.CRASH]
         self.result = result
         self.reason = reason
@@ -39,17 +40,22 @@ def track(sesh: WebDriverSession, tracking_nums, executeScript):
             )
             try:
                 curr_result = executeScript(sesh, tracking_num)
-                if curr_result == result.SUCCESS:
+                if curr_result == result.RETRY:
+                    continue
+                elif curr_result == result.SUCCESS:
                     report["success"].append((tracking_num, curr_result))
                     logger.info("success")
                     break
                 else:
                     report["fail"].append((tracking_num, curr_result))
                     logger.info("failed")
+                    break
             except Exception as e:
                 curr_result = result(result.CRASH, "Unknown error occured")
                 logger.debug("(#{}) Unknown error: {}".format(tracking_num, e))
-                logger.info("unknown error encountered :(")
+                logger.info("unknown error encountered, retrying")
                 report["crash"].append((tracking_num, curr_result))
-        sleep(3)
+
+            sleep(1)
+        sleep(2)
     return report
