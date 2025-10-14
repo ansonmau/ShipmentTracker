3a26478e12fb9651dfa08041eefe8b2e17a582c9
ansonmau <ansonmau172@gmail.com>
2025-10-14 17:03:25 -0400

    add settings to git

---
+++ core/settings.py
@@ -0,0 +1,17 @@
+class Settings:
+    clear_downloads = False
+
+    scrape = {
+            "eshipper": True,
+            "ems": True,
+            "freightcom": True,
+            }
+
+    track = {
+            "canadapost": True,
+            "canpar": True,
+            "fedex": True,
+            "purolator": True,
+            "ups": True,
+            }
+
