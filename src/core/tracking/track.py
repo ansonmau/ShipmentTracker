f1604ecd007cc2152a85242957bb044543ff393c
ansonmau <ansonmau172@gmail.com>
2026-08-26 20:16:00 -0400

    Slight fixes
    feat: random delay between new page loads
    style: status message format change
    style: spacing

--- src/core/tracking/track.py
+++ src/core/tracking/track.py
@@ -4,29 +4,34 @@ from time import sleep
 from src.core.settings import Settings
 
 import src.core.tracking.scripts.canadapost as cp
-import src.core.tracking.scripts.ups as ups
-import src.core.tracking.scripts.fedex as fdx
-import src.core.tracking.scripts.canpar as cpr
-import src.core.tracking.scripts.purolator as pur
+import src.core.tracking.scripts.ups        as ups
+import src.core.tracking.scripts.fedex      as fdx
+import src.core.tracking.scripts.canpar     as cpr
+import src.core.tracking.scripts.purolator  as pur
+
+import src.core.utils as utils
+
+
 
 logger = getLogger("tracker")
 
 class Handler:
     def __init__(self, wds, data_handler, report) -> None:
-        self.wds        =   wds
-        self.driver     =   self.wds.driver
-        self.dh         =   data_handler 
-        self.report     =   report
+        self.wds    = wds
+        self.driver = self.wds.driver
+        self.dh     = data_handler
+        self.report = report
 
-        self.wait_between_attempts_s     =   2
-        self.wait_between_shipments_s    =   3
+        # delay range between new page loads (retries / next package)
+        self.delay_low = 1
+        self.delay_high = 8
 
         self.scripts = {
                 "canada post": cp.executeScript,
-                "ups": ups.executeScript,
-                "fedex": fdx.executeScript,
-                "canpar": cpr.executeScript,
-                "purolator": pur.executeScript,
+                "ups":         ups.executeScript,
+                "fedex":       fdx.executeScript,
+                "canpar":      cpr.executeScript,
+                "purolator":   pur.executeScript,
                 }
 
     def run(self):
@@ -49,22 +54,22 @@ class Handler:
 
                 # log msg list that will be joined by \t and then info'd
                 # [n]carrier#tracking_number   n%  attempt n/3
-                log_msg_l = [f"{carrier}#{tracking_num}", f"{int((comp_shipment_count / total_shipment_count)*100)}%"]
+                log_msg_l = [f"{carrier}", f"#{tracking_num}", f"~{int((comp_shipment_count / total_shipment_count)*100)}%"]
                 if (attempt_count > 1):
                     log_msg_l.append(f"attempt {attempt_count}")
-                logger.info("  ---  ".join(log_msg_l))
+                logger.info(" | ".join(log_msg_l))
 
                 try:
                     curr_result = self.scripts[carrier](self.wds, tracking_num)
                 except Exception as e:
                     if (attempt_count == 3):
                         curr_result = Result(Result.CRASH, carrier=carrier, reason="Unknown error occured", tracking_number=tracking_num)
-                        logger.debug("(#{}) Unknown error: {}".format(tracking_num, e))
+                        logger.debug("Unknown error: {}".format(tracking_num, e))
                     else:
                         curr_result = Result(Result.RETRY)
                 if curr_result.result != Result.RETRY:
                     break
-                sleep(self.wait_between_attempts_s)
+                utils.rand_wait(self.delay_low, self.delay_high)
 
             if (curr_result and curr_result.result == Result.RETRY):
                 curr_result.set_result(Result.FAIL)
@@ -73,4 +78,5 @@ class Handler:
             logger.info(str(curr_result))
             self.report.add_result(curr_result)
             comp_shipment_count += 1
-            sleep(self.wait_between_shipments_s)
+            utils.rand_wait(self.delay_low, self.delay_high)
+
