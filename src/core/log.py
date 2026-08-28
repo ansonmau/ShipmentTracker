aa472103092d5361d501d6ded86824ca08550b9f
ansonmau <ansonmau172@gmail.com>
2026-08-27 20:03:29 -0400

    feat: static method for console output

--- src/core/log.py
+++ src/core/log.py
@@ -3,16 +3,13 @@ from datetime import datetime
 import os
 from src.core.utils import create_file
 
-logging.basicConfig(
-    format="%(asctime)s %(name)s: [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler()],
-)
-
 class MyLogger:
-    _time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    _log_file_path = os.path.join("./logs", "{}.log".format(_time))
-    _level = logging.INFO
-    _loggers = []
+    _time           = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    _log_file_path  = os.path.join("./logs", "{}.log".format(_time))
+    _level          = logging.INFO
+    _loggers        = []
+    _console_out    = False
+    _default_format = "%(levelname)s/%(name)s: %(message)s"
 
     def __init__(self, name):
         self._name = name
@@ -46,9 +43,11 @@ class MyLogger:
                 "debug": logging.DEBUG,
                 "info":  logging.INFO,
                 }
+        choice = levels[level_name]
 
+        MyLogger._level = choice
         for logger in MyLogger._loggers:
-            logger.set_level(levels[level_name])
+            logger.set_level(choice)
 
     @staticmethod
     def init_file_handler():
@@ -57,9 +56,18 @@ class MyLogger:
             logger._logger.addHandler(logging.FileHandler(MyLogger._log_file_path))
 
     @staticmethod
+    def enable_console_output():
+        sh = logging.StreamHandler()
+        sh.setFormatter(logging.Formatter(MyLogger._default_format))
+        sh.setLevel(MyLogger._level)
+        for logger in MyLogger._loggers:
+            logger._logger.addHandler(sh)
+
+    @staticmethod
     def mute_noisy_libs():
         for noisy_lib in ["urllib3", "selenium", "botocore", "undetected_chromedriver"]:
             logging.getLogger(noisy_lib).setLevel(logging.WARNING)
+
 
 def getLogger(name):
     return MyLogger(name)
