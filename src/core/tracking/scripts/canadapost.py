7eab24f31fa36bba736a04617d097891aec34f58
ansonmau <ansonmau172@gmail.com>
2026-08-27 20:03:43 -0400

    Clean up on CanadaPost script
    feat: check for label not being created yet (no associated tracking num)
    feat: new logic for quickly getting the status of the page (delivered, not ready, etc.)
    feat: better debugging output for testing
    style: separators with titles for locators and functions

--- src/core/tracking/scripts/canadapost.py
+++ src/core/tracking/scripts/canadapost.py
@@ -10,16 +10,21 @@ from time import sleep
 logger = getLogger("canada-post")
 
 locators = {
+    # ── general ───────────────────────────────────────────────────────────
     "get_email_notif":   Locator(ElementTypes.css, ".trackEmail"),
     "buttons":           Locator(ElementTypes.css, ".button"),
+    # ── dialog box ────────────────────────────────────────────────────────
     "email_input":       Locator(ElementTypes.id, "emailAddressInput"),
     "add_email_btn":     Locator(ElementTypes.css, ".add"),
-    "add_email_blocked": Locator(ElementTypes.css, ".disabled"),
     "submit_btn":        Locator(ElementTypes.id, "submitButton"),
+    # ── dialog types ────────────────────────────────────────────────────────
     "dialog_type_1":     Locator(ElementTypes.tag, "track-email-dialog"),
     "dialog_type_2":     Locator(ElementTypes.tag, "add-emails-dialog"),
+    # ── checking ──────────────────────────────────────────────────────────
     "error_msg":         Locator(ElementTypes.id, "errorModal"),
     "tracking_status":   Locator(ElementTypes.tag, "track-expected-delivery"),
+    "add_email_blocked": Locator(ElementTypes.css, ".disabled"),
+    "list_of_packages":  Locator(ElementTypes.tag, "list-of-packages"),
 }
 
 def executeScript(wds, tracking_num):
@@ -36,17 +41,23 @@ def executeScript(wds, tracking_num):
 
     wds.nav.get(link)
 
-    if (check.is_delivered()):
-        result.set_reason("Package already delivered")
-        return result
-
     if (check.full_block()):
+        logger.debug("Error message (likely bot detection)")
         result.set_reason("Error message (likely bot detection)")
         return result
-    
+
     if (check.notif_btn_missing()):
-        result.set_reason("Notification button not found")
+        # figure out why it's missing
+        if (check.is_delivered()):
+            msg = "Package already delivered"
+        elif (check.no_associated_item()):
+            msg = "Label not created yet (No associated number)"
+        else:
+            msg = "Notification button not found"
+        logger.debug(f"Failed. reason: {msg}")
+        result.set_reason(msg)
         return result
+    
 
     wds.click.by_locator(locators["get_email_notif"])
 
@@ -159,45 +170,101 @@ class StateCheck:
         self.driver = wds.driver 
         self.dialog = DialogHandler(wds)
 
+    # ─────────────────────────────< bot detect >─────────────────────────────
     def full_block(self):
-        full_block_modal = self.wds.find.element(locators['error_msg'])
+        # can do a 1 sec wait here since it'll be perma on the screen when it does show up
+        logger.debug("Searching for error box")
+        full_block_modal = self.wds.find.element(locators['error_msg'], wait=1)
 
-        if not full_block_modal:
+        if (full_block_modal is None) or (not full_block_modal.is_displayed()):
+            logger.debug("... no error found")
             return False
 
+        logger.debug("Error box detected. Attempting to get past it.")
         try:
             # attempt to click OK to get rid of it
-            if full_block_modal.is_displayed():
-                    error_box = self.wds.find.element(locators['error_msg'])
-                    okay_button = self.wds.find.buttons_within(error_box, filter="OK")[0]
-                    self.wds.click.element(okay_button)
-                    sleep(2)
-                
-                    if self.wds.find.element(locators['error_msg']):
-                        return True
-                    else:
-                        return False
+            error_box = self.wds.find.element(locators['error_msg'])
+            okay_button = self.wds.find.buttons_within(error_box, filter="OK")[0]
+            self.wds.click.element(okay_button)
+            sleep(2)
+        
+            if self.wds.find.element(locators['error_msg']):
+                logger.debug("... attempt failed.")
+                return True
+            else:
+                logger.debug("... success.")
+                return False
         except:
+            logger.debug("... failed.")
             pass 
 
         return True
 
 
+    # ────────────────────────────< trackability >────────────────────────────
     def notif_btn_missing(self):
+        """
+        check for if notification button is missing
+        """
+        
+        logger.debug("Searching for notification button")
         btn = self.wds.find.element(locators["get_email_notif"], wait = 3)
-        return False if btn else True
-
-    def dialog_is_loaded(self):
-        return "Get email notifications" in self.dialog.get_text()
+        if btn:
+            logger.debug("... found")
+            return False
+        else:
+            logger.debug("... missing")
+            return True
 
     def is_delivered(self):
+        """
+        checks if delivered is in the status text
+        """
+        logger.debug("Checking delivery status")
         w = self.wds
-        tracking_status = w.find.element(locators["tracking_status"])
-        if not tracking_status:
-            logger.info("Failed to read tracking status, continuing...")
-            return
+        tracking_status = w.find.element(locators["tracking_status"], wait=1)
 
-        return "delivered" in w.read.element_text( tracking_status ).lower()
+        if not tracking_status:
+            logger.debug("... failed to find tracking status, skipping.")
+            return False
+
+        logger.debug("...delivery text located")
+        if "delivered" in w.read.element_text( tracking_status ).lower():
+            logger.debug("... already delivered")
+            return True
+        else:
+            logger.debug("... not delivered yet")
+            return False
+
+
+    def no_associated_item(self):
+        """
+        - happens when label isn't created yet
+        - checks if identifying text exists on the part of the page
+        that contains shipment details. 
+        """
+        text_to_search = "item associated with this number"
+
+        logger.debug("Checking if label is created")
+        w = self.wds
+        elm = w.find.element(locators["list_of_packages"], wait=1)
+
+        if elm is None:
+            logger.debug("... unable to locate element, skipping.")
+            # assume its not due to this issue
+            return False
+
+        text = w.read.element_text(elm)
+        if text_to_search in text:
+            logger.debug("... not created")
+            return True
+        else:
+            logger.debug("... created")
+            return False
+
+    # ───────────────────────────< In dialog box >─────────────────────────
+    def dialog_is_loaded(self):
+        return "Get email notifications" in self.dialog.get_text()
 
     def max_emails_reached(self):
         if "reached the maximum" in self.dialog.get_text():
